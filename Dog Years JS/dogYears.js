4d30d256034dde811d8ee02daa5141e7339d8338
makers <bbiehn1@gmail.com>
2020-01-29 12:43:43 -0600

    updated project dog years

--- Dog Years JS/dogYears.js
+++ Dog Years JS/dogYears.js
@@ -15,4 +15,4 @@ let myAgeInDogYears = earlyYears + laterYears;
 const myName = 'makers'.toLowerCase();
 
 // human age in dog years
-console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears}.`);
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years.`);
