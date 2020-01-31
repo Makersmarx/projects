7f85ab64c595dcd57060bce6688d30b74dd25e30
makers <bbiehn1@gmail.com>
2020-01-31 17:56:24 -0600

    rock paper scissors game added

---
+++ Rock Paper Scissors JS/rps.js
@@ -0,0 +1,51 @@
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    return userInput;
+  } else {
+    console.log('invalid choice');
+  };
+};
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3)
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+      break;
+    case 1:
+      return 'paper';
+      break;
+    case 2:
+      return 'scissors';
+      break;
+    default:
+      console.log('invalid choice');
+      break;
+  }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return 'Its a tie';
+  } else if (userChoice === 'rock' && computerChoice === 'paper') {
+    return 'computer wins';
+  } else if (userChoice === 'paper' && computerChoice === 'scissors') {
+    return 'computer wins';
+  } else if (userChoice === 'scissors' && computerChoice === 'rock') {
+    return 'computer wins';
+  } else {
+    return 'your the winner';
+  }
+}
+
+const playGame = () => {
+  const userChoice = getUserChoice('rock');
+  const computerChoice = getComputerChoice();
+  console.log('You picked: ' + userChoice);
+  console.log('The computer picked: ' + computerChoice);
+
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
